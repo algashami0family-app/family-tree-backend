952176c30d86467752618be9f6c68ff2e2b2b75f
Turki-Algashami <turkialgashami@gmail.com>
2026-03-07 23:14:22 +0300

    add addNewSpouse endpoint

--- routes/spouseRoutes.js
+++ routes/spouseRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const { addSpouse, getSpouses, deleteSpouse } = require('../controllers/spouseController');
+const { addSpouse, addNewSpouse, getSpouses, deleteSpouse } = require('../controllers/spouseController');
 
+router.post('/new', protect, addNewSpouse);
 router.post('/', protect, addSpouse);
 router.get('/:memberId', protect, getSpouses);
 router.delete('/:id', protect, deleteSpouse);
